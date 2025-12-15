9aab8b4bff9b2f5e3707bf0aac9775da7e0c5c82
MikhailY <sarmetr@ya.ru>
2025-12-15 15:20:02 +0400

    Create form auth

--- apple-app/backend/views/apple/auth.php
+++ apple-app/backend/views/apple/auth.php
@@ -1,1 +1,10 @@
 <?php
+use yii\helpers\Html;
+?>
+
+<h2>Доступ к бэкенду</h2>
+<p>Введите пароль для доступа:</p>
+<form method="get">
+    <?= Html::input('password', 'password', null, ['required' => true]) ?>
+    <?= Html::submitButton('Войти', ['class' => 'btn btn-primary']) ?>
+</form>
